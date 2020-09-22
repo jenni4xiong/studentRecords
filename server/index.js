a5b4a931d7218f5e7486554e8aeb32079ad5436d
Jennifer Xiong <jenni4xiong@gmail.com>
2020-09-21 22:36:14 -0700

    added comments on todo

--- server/index.js
+++ server/index.js
@@ -2,6 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+/* 
+Build a single RESTful microservice accessible over HTTP 
+(HTTPS is not required) that provides basic CRUD methods 
+to list, get, create, update, and delete student records. 
+The list method must support pagination 
+(specifying an index, count, and sort order) 
+and should include an x-total-count header 
+so that the client understands the total number of records available. 
+The RESTful API should support basic authentication
+ using a simple username and token (can be hardwired). 
+ The student database should be populated with approximately 200 records 
+ (fake data with non-sensical fields is fine). 
+ The microservice can be built using any framework and/or language.
+*/
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
