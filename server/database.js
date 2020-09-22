a32f2c3e5cfc9ddcd599500a3e5d93b42e4af4ca
Jennifer Xiong <jenni4xiong@gmail.com>
2020-09-22 02:33:28 -0700

    deleted old get request

--- server/database.js
+++ server/database.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/studentRecords', { useNewUrlParser: true, useUnifiedTopology: true });
 const Schema = mongoose.Schema;
 
-//schema -> id, name, grade, gender, age
 const studentSchema = new Schema({
   name: String,
   grade: Number,
@@ -12,10 +11,6 @@ const studentSchema = new Schema({
 
 const Student = mongoose.model('student', studentSchema)
 
-const getAllStudents = (page, limit) => {
-  return Student.find({});
-}
-
 const addStudent = (student) => {
   return Student.create(student);
 }
@@ -24,4 +19,4 @@ const deleteStudent = (student) => {
   return Student.deleteOne({ _id: student._id });
 }
 
-module.exports = { getAllStudents, addStudent, deleteStudent, Student };
+module.exports = { Student, addStudent, deleteStudent };
