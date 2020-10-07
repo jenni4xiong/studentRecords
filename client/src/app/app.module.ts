5cba06cfabe6d18c81de169a35cf5c9c573e32f8
Jennifer Xiong <jenni4xiong@gmail.com>
2020-10-07 09:48:40 -0700

    connect student profile

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { BodyComponent } from './main/body/body.component';
+import { AddStudentComponent } from './main/body/add-student/add-student.component';
+import { StudentListComponent } from './main/body/student-list/student-list.component';
+import { StudentProfileComponent } from './main/body/student-profile/student-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    BodyComponent,
+    AddStudentComponent,
+    StudentListComponent,
+    StudentProfileComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
