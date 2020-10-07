8d1fb9a2b261a38c6d9c90cc97c768b6ab200907
Jennifer Xiong <jenni4xiong@gmail.com>
2020-10-06 17:19:28 -0700

    removed comments

--- server/index.js
+++ server/index.js
@@ -9,21 +9,6 @@ const auth = require('./auth.js');
 app.use(bodyParser.json());
 app.use(auth);
 
-/* 
-Build a single RESTful microservice accessible over HTTP 
-(HTTPS is not required) that provides basic CRUD methods 
-to list, get, create, update, and delete student records. 
-The list method must support pagination 
-(specifying an index, count, and sort order) 
-and should include an x-total-count header 
-so that the client understands the total number of records available. 
-The RESTful API should support basic authentication
- using a simple username and token (can be hardwired). 
- The student database should be populated with approximately 200 records 
- (fake data with non-sensical fields is fine). 
- The microservice can be built using any framework and/or language.
-*/
-
 app.get('/students', pagination(db.Student), (req, res) => {
   res.set('x-total-count', res.totalCount);
   res.send({ studentRecords: res.paginatedResults });
