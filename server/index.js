724ebab2ad9002d8daada8de31aa4f891841e73c
Jennifer Xiong <jenni4xiong@gmail.com>
2020-10-06 17:20:03 -0700

    changed log for app.listen

--- server/index.js
+++ server/index.js
@@ -39,5 +39,5 @@ app.delete('/students/:id', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}`)
 });
