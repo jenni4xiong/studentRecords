6531328dd7d8aed28621d9ca503de828db49f2aa
Jennifer Xiong <jenni4xiong@gmail.com>
2020-10-14 12:26:32 -0700

    changed file path for components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,10 +5,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
-import { BodyComponent } from './main/body/body.component';
-import { AddStudentComponent } from './main/body/add-student/add-student.component';
-import { StudentListComponent } from './main/body/student-list/student-list.component';
-import { StudentProfileComponent } from './main/body/student-profile/student-profile.component';
+import { BodyComponent } from './body/body.component';
+import { AddStudentComponent } from './add-student/add-student.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { StudentProfileComponent } from './student-profile/student-profile.component';
 
 @NgModule({
   declarations: [
